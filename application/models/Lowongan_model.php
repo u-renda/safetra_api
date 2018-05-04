dbe92094e55a2dbac21b8a4a4b7fe8bead9bf336
Renda <renda86@yahoo.com>
2018-05-04 19:00:44 +0700

    tambah lowongan

---
+++ application/models/Lowongan_model.php
@@ -0,0 +1,72 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Lowongan_model extends CI_Model {
+
+    var $table = 'lowongan';
+	var $table_id = 'id_lowongan';
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function create($param)
+    {
+        $this->db->set($this->table_id, 'UUID_SHORT()', FALSE);
+		$query = $this->db->insert($this->table, $param);
+		return $query;
+    }
+    
+    function delete($id)
+    {
+        $this->db->where($this->table_id, $id);
+        $query = $this->db->delete($this->table);
+        return $query;
+    }
+    
+    function info($param)
+    {
+        $where = array();
+        if (isset($param['id_lowongan']) == TRUE)
+        {
+            $where += array('id_lowongan' => $param['id_lowongan']);
+        }
+        
+        $this->db->select('id_lowongan, name, description, created_date, updated_date');
+        $this->db->from($this->table);
+        $this->db->where($where);
+        $query = $this->db->get();
+        return $query;
+    }
+    
+    function lists($param)
+    {
+        $where = array();
+        
+        $this->db->select('id_lowongan, name, description, created_date, updated_date');
+        $this->db->from($this->table);
+        $this->db->where($where);
+        $this->db->order_by($param['order'], $param['sort']);
+        $this->db->limit($param['limit'], $param['offset']);
+        $query = $this->db->get();
+        return $query;
+    }
+    
+    function lists_count($param)
+    {
+        $where = array();
+        
+        $this->db->select($this->table_id);
+        $this->db->from($this->table);
+        $this->db->where($where);
+        $query = $this->db->count_all_results();
+        return $query;
+    }
+    
+    function update($id, $param)
+    {
+        $this->db->where($this->table_id, $id);
+        $query = $this->db->update($this->table, $param);
+        return $query;
+    }
+}
